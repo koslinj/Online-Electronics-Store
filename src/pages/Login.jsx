ad46a044189ed3bbe7f28a17dfa8f1d432ebf9ef
koslinj <koslinj@gmail.com>
2024-03-12 19:00:58 +0100

    improve login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../providers/AuthProvider";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const Login = () => {
   const { setToken } = useAuth();
@@ -9,21 +11,12 @@ export const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch("http://localhost:8080/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
+      const response = await axios.post("http://localhost:8080/api/auth/login", {
+        email,
+        password,
       });
 
-      if (!response.ok) {
-        console.error("Authentication failed");
-        return;
-      }
-
-      const data = await response.json();
-      const token = data.token;
+      const token = response.data.token;
 
       setToken(token);
       // navigate("/profile", { replace: true });
@@ -33,26 +26,37 @@ export const Login = () => {
   };
 
   return (
-    <div>
-      <label>Email:</label>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+    <div className="flex flex-col items-center">
+      <div className="shadow-card rounded-lg p-4 flex flex-col gap-8 my-6 w-72">
+        <h2 className="font-bold text-2xl">Zaloguj się</h2>
+        <input
+          className='py-1 px-2 outline-none border-gray-700 border-2 rounded-md'
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-      <br />
+        <input
+          className='py-1 px-2 outline-none border-gray-700 border-2 rounded-md'
+          type="password"
+          placeholder="Hasło"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-      <label>Password:</label>
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+        <button onClick={handleLogin} className="bg-green-700 text-white rounded-lg p-2">
+          Zaloguj się
+        </button>
+      </div>
 
-      <br />
-
-      <button onClick={handleLogin}>Login</button>
+      <h2 className="text-xl mb-4">Nie masz konta?</h2>
+      <Link to="/register">
+        <button
+          className="border-green-700 text-green-700 border-2 rounded-lg p-2 hover:scale-110 hover:bg-green-700 hover:text-white duration-200">
+          Załóż konto
+        </button>
+      </Link>
     </div>
   );
 };
