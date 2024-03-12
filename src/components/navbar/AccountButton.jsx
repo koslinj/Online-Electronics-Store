36fa69e8999c0a0fa62d71326ce76d3952526001
koslinj <koslinj@gmail.com>
2024-03-12 10:46:28 +0100

    fix link on account button

--- src/components/navbar/AccountButton.jsx
+++ src/components/navbar/AccountButton.jsx
@@ -1,12 +1,13 @@
 import { GoPerson } from "react-icons/go";
+import { Link } from "react-router-dom";
 
 export const AccountButton = () => {
   return (
-    <a href="profile">
+    <Link to="/profile">
       <div className="flex flex-col justify-center items-center cursor-pointer rounded-md p-2 hover:bg-green-200">
         <GoPerson className="w-6 h-6" />
         <p className="text-xs">Twoje konto</p>
       </div>
-    </a>
+    </Link>
   )
 }
