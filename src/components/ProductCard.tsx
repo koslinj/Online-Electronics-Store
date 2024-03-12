299409239f38c0c45a554ff86fcae4e8e89c39be
koslinj <koslinj@gmail.com>
2024-03-12 22:46:11 +0100

    update displaying price in ProductCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -6,11 +6,19 @@ interface Props {
 }
 
 export const ProductCard = ({ product }: Props) => {
+  const formattedPrice = product.price.toLocaleString('pl-PL', {
+    style: 'currency',
+    currency: 'PLN',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+    useGrouping: true,
+  });
+
   return (
     <div className="group relative w-52 rounded-md flex flex-col items-center p-2 pb-16 h-96 before:absolute before:h-[1px] before:w-48 before:bottom-0 before:bg-gray-300 cursor-pointer hover:shadow-card duration-200">
       <img className='w-full' src={product.imageUrl} alt={product.name} />
       <p className="w-full text-sm">{product.name}</p>
-      <p className="absolute bottom-2 left-2">2 649,00 zł</p>
+      <p className="absolute bottom-2 left-2">{formattedPrice}</p>
       <div className="absolute right-3 bottom-3 opacity-0 group-hover:opacity-100 duration-200 text-green-600 rounded-lg border-2 border-green-600 p-1 hover:bg-green-600 hover:text-white">
         <MdOutlineAddShoppingCart className="w-5 h-5" />
       </div>
