134bf2c088ff09bcdad67a27dc7147918f55ac7b
koslinj <koslinj@gmail.com>
2024-03-12 19:01:20 +0100

    root component (navbar on each page)

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,8 +1,31 @@
 import { TopNavbar } from '../components/navbar/TopNavbar'
 import { BottomNavbar } from '../components/navbar/BottomNavbar'
 import { Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react';
 
 export const Root = () => {
+  // const [side, setSide] = useState("left");
+
+  // useEffect(() => {
+  //   const handleMouseMove = (e) => {
+  //     const mouseX = e.clientX;
+
+  //     const threshold = window.innerWidth / 2;
+
+  //     if (mouseX <= threshold) {
+  //       setSide("right");
+  //     } else {
+  //       setSide("left");
+  //     }
+  //   };
+
+  //   window.addEventListener("mousemove", handleMouseMove);
+
+  //   return () => {
+  //     window.removeEventListener("mousemove", handleMouseMove);
+  //   };
+  // }, []);
+
   return (
     <>
       <TopNavbar />
