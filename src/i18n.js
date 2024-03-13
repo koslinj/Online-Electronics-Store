caba96e4a736593c139d74cd4c47bead2dcd8e92
koslinj <koslinj@gmail.com>
2024-03-13 21:33:28 +0100

    login page translations

--- src/i18n.js
+++ src/i18n.js
@@ -13,7 +13,11 @@ i18next
           admin: "Admin",
           yourAccount: "Your account",
           cart: "Cart",
-          searchPlaceholder: "What are you looking for?"
+          searchPlaceholder: "What are you looking for?",
+          login: "Log in",
+          noAccount: "Do not have account?",
+          createAccount: "Create an account",
+          password: "Password"
         }
       },
       pl: {
@@ -21,7 +25,11 @@ i18next
           admin: "Administrator",
           yourAccount: "Twoje konto",
           cart: "Koszyk",
-          searchPlaceholder: "Czego szukasz?"
+          searchPlaceholder: "Czego szukasz?",
+          login: "Zaloguj się",
+          noAccount: "Nie masz konta?",
+          createAccount: "Załóż konto",
+          password: "Hasło"
         }
       }
     }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { useAuth } from "../providers/AuthProvider";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { useTranslation } from "react-i18next";
 
 export const Login = () => {
+  const { t } = useTranslation()
   const { setToken } = useAuth();
 
   const [email, setEmail] = useState("");
@@ -28,7 +30,7 @@ export const Login = () => {
   return (
     <div className="flex flex-col items-center">
       <div className="shadow-card rounded-lg p-4 flex flex-col gap-8 my-6 w-72">
-        <h2 className="font-bold text-2xl">Zaloguj się</h2>
+        <h2 className="font-bold text-2xl">{t("login")}</h2>
         <input
           className='py-1 px-2 outline-none border-gray-700 border-2 rounded-md'
           type="email"
@@ -40,21 +42,21 @@ export const Login = () => {
         <input
           className='py-1 px-2 outline-none border-gray-700 border-2 rounded-md'
           type="password"
-          placeholder="Hasło"
+          placeholder={t("password")}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
 
         <button onClick={handleLogin} className="bg-green-700 text-white rounded-lg p-2">
-          Zaloguj się
+          {t("login")}
         </button>
       </div>
 
-      <h2 className="text-xl mb-4">Nie masz konta?</h2>
+      <h2 className="text-xl mb-4">{t("noAccount")}</h2>
       <Link to="/register">
         <button
           className="border-green-700 text-green-700 border-2 rounded-lg p-2 hover:scale-110 hover:bg-green-700 hover:text-white duration-200">
-          Załóż konto
+          {t("createAccount")}
         </button>
       </Link>
     </div>
