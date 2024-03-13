1b7403971db81fefe41b6111fc51037eb7c368f7
koslinj <koslinj@gmail.com>
2024-03-13 21:44:19 +0100

    improve LanguageButtons

--- src/components/navbar/LanguageButtons.jsx
+++ src/components/navbar/LanguageButtons.jsx
@@ -14,7 +14,7 @@ export const LanguageButtons = () => {
         <div className="border-black border-2 rounded-full">
           <img src={plIcon} alt="Poland flag" width={24} />
         </div>
-        <p className="leading-3 text-xs">PL</p>
+        <p className={`leading-3 text-gray-700 text-sm ${i18n.language == "pl" && "font-extrabold"}`}>PL</p>
       </button>
       <button
         onClick={() => i18n.changeLanguage('en')}
@@ -23,7 +23,7 @@ export const LanguageButtons = () => {
         <div className="border-black border-2 rounded-full">
           <img src={gbIcon} alt="Great Britain flag" width={24} />
         </div>
-        <p className="leading-3 text-xs">EN</p>
+        <p className={`leading-3 text-gray-700 text-sm ${i18n.language == "en" && "font-extrabold"}`}>EN</p>
       </button>
     </div>
   )
