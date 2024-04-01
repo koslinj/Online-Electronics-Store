97ec85b088ee803ed56544b65f0c2bf5ef14e15a
koslinj <koslinj@gmail.com>
2024-04-01 19:10:26 +0200

    separated components and finish adding to cart from product page

---
+++ src/components/productPage/CartPart.tsx
@@ -0,0 +1,68 @@
+import { useCart } from '@/providers/CartProvider';
+import { Product } from '@/types';
+import React, { useState } from 'react'
+import { FaMinus, FaPlus } from "react-icons/fa";
+import { message } from "antd";
+import { MdError, MdOutlineAddShoppingCart } from "react-icons/md";
+import { useTranslation } from 'react-i18next';
+
+interface Props {
+  product: Product | undefined
+}
+
+export const CartPart = ({ product }: Props) => {
+  const { t } = useTranslation()
+  const { add } = useCart();
+  const [count, setCount] = useState(1)
+
+  const handleAddToCart = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    add(product!, count)
+  }
+
+  const handleMinus = () => {
+    if (count > 1) {
+      setCount(prev => prev - 1)
+    } else {
+      message.error({
+        content:
+          <div className='flex items-center gap-3'>
+            <MdError className='size-10 text-red-500' />
+            <p className='text-xl'>{t('badProdCount')}</p>
+          </div>,
+        icon: <></>
+      });
+    }
+  }
+
+  const handlePlus = () => {
+    setCount(prev => prev + 1)
+  }
+
+  return (
+    <div className="flex gap-5">
+      <div className="flex items-center gap-1 px-1 rounded-lg border-2 border-gray-300">
+        <button
+          className='hover:bg-gray-200 rounded-md duration-150 flex justify-center items-center p-2'
+          onClick={handleMinus}
+        >
+          <FaMinus className='size-5' />
+        </button>
+        <p className='text-lg w-7 font-semibold rounded-md text-center'>{count}</p>
+        <button
+          className='hover:bg-gray-200 rounded-md duration-150 flex justify-center items-center p-2'
+          onClick={handlePlus}
+        >
+          <FaPlus className='size-5' />
+        </button>
+      </div>
+      <button
+        onClick={handleAddToCart}
+        className="p-3 flex items-center justify-center gap-3 duration-200 rounded-lg bg-green-600 hover:bg-green-800 text-white"
+      >
+        <MdOutlineAddShoppingCart className="size-7" />
+        <p>{t('addToCart')}</p>
+      </button>
+    </div>
+  )
+}
--- src/components/productPage/PriceAndCart.tsx
+++ src/components/productPage/PriceAndCart.tsx
@@ -1,8 +1,6 @@
-import { useMemo } from "react";
 import { Product } from "@/types";
-import { MdOutlineAddShoppingCart } from "react-icons/md";
-import { useCart } from "@/providers/CartProvider";
 import { useTranslation } from "react-i18next";
+import { CartPart } from "./CartPart";
 
 interface Props {
   product: Product | undefined
@@ -10,7 +8,6 @@ interface Props {
 
 export const PriceAndCart = ({ product }: Props) => {
   const { t } = useTranslation()
-  const { add } = useCart();
 
   const formattedPrice = product?.price.toLocaleString('pl-PL', {
     style: 'currency',
@@ -20,23 +17,12 @@ export const PriceAndCart = ({ product }: Props) => {
     useGrouping: true,
   });
 
-  const handleAddToCart = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    add(product!, 3)
-  }
-
   return (
-    <div className="p-1 md:p-5 border-2 border-gray-400 rounded-xl flex flex-col text-right">
+    <div className="p-1 md:p-5 border-2 border-gray-400 rounded-xl flex gap-y-1 flex-col text-right">
       <p className="text-2xl">{formattedPrice}</p>
       <p>{t('lowest')}</p>
-      <p className="text-gray-700 line-through">{formattedPrice}</p>
-      <button
-        onClick={handleAddToCart}
-        className="p-3 flex items-center justify-center gap-3 duration-200 rounded-lg bg-green-600 hover:bg-green-800 text-white"
-      >
-        <MdOutlineAddShoppingCart className="size-7" />
-        <p>{t('addToCart')}</p>
-      </button>
+      <p className="text-gray-700 line-through mb-6">{formattedPrice}</p>
+      <CartPart product={product} />
     </div>
   )
 }
