392983df3ef8d0529fdeecec65d9ef1362400307
koslinj <koslinj@gmail.com>
2024-04-01 22:14:37 +0200

    empty cart header

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -10,7 +10,10 @@ export const Cart = () => {
   return (
     <div className="max-w-4xl">
       <div className="mb-2 mt-6 flex justify-between">
-        <h1 className="text-4xl font-semibold">{t('cart')}</h1>
+        <div>
+          <h1 className="text-4xl font-semibold">{t('cart')}</h1>
+          {cart.length < 1 && <h2 className="text-2xl mt-2">{t('emptyCart')}</h2>}
+        </div>
         {cart.length > 0 && (
           <button onClick={clearCart} className="flex items-center gap-2 hover:bg-gray-200 p-2 rounded-md duration-150">
             <BsTrash className="size-6" />
