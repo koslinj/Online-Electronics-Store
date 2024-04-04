a9060610953c379c5feba5ee65d63db021ead451
koslinj <koslinj@gmail.com>
2024-04-04 22:26:14 +0200

    begin of order summary

--- src/pages/order/Order.tsx
+++ src/pages/order/Order.tsx
@@ -2,11 +2,16 @@ import { DeliveryAddress } from '@/components/navbar/order/DeliveryAddress'
 import { DeliveryMethod } from '@/components/navbar/order/DeliveryMethod'
 import { Payment } from '@/components/navbar/order/Payment';
 import { useAuth } from '@/providers/AuthProvider';
+import { useCart } from '@/providers/CartProvider';
 import { User } from '@/types';
 import axios from 'axios';
 import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 export const Order = () => {
+  const { t } = useTranslation()
+  const { cart } = useCart()
   const [user, setUser] = useState<User>();
   const { setToken } = useAuth()
 
@@ -48,7 +53,18 @@ export const Order = () => {
               <Payment />
             </div>
             <div>
-              Przejdź do podsumowania
+              <p>Przejdź do podsumowania</p>
+              {cart.map((item) => (
+                <div key={item.product.id} className="rounded-lg p-2 flex gap-x-2 items-center w-72">
+                  <img className="w-20" src={item.product.imageUrl} alt={item.product.name} />
+                  <div>
+                    <Link to={`/products/${item.product.categoryUrl}/${encodeURIComponent(item.product.name)}`}>
+                      <p className="font-semibold hover:underline">{item.product.name}</p>
+                    </Link>
+                    <p className="mt-2 text-blue-900">{item.quantity} {t('pieces')}</p>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
