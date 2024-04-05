d76df7741dbdc59de22132eb4b3db4f605703946
koslinj <koslinj@gmail.com>
2024-04-05 11:28:49 +0200

    connection between order and summary by localStorage

--- src/components/order/DeliveryMethod.tsx
+++ src/components/order/DeliveryMethod.tsx
@@ -1,5 +1,5 @@
 import { ConfigProvider, Radio, RadioChangeEvent, Space } from 'antd'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MyRadio } from './MyRadio';
 import transportIcon from "@/images/transport_icon.png"
 import inpostIcon from "@/images/inpost_icon.png"
@@ -8,24 +8,38 @@ import shopIcon from "@/images/shop_icon.png"
 const contents = [
   {
     title: "Kurier – InPost, UPS, FedEx, DTS, PickPack",
-    desc: "Dostawa w ciągu 5 dni roboczych"
+    desc: "Dostawa w ciągu 5 dni roboczych",
+    icon: transportIcon
   },
   {
     title: "Odbiór w salonie ELECTROstore",
-    desc: "Dostawa na jutro"
+    desc: "Dostawa na jutro",
+    icon: shopIcon
   },
   {
     title: "InPost Paczkomat® 24/7",
-    desc: "Dostawa w ciągu 5 dni roboczych"
+    desc: "Dostawa w ciągu 5 dni roboczych",
+    icon: inpostIcon
   }
 ]
 
 export const DeliveryMethod = () => {
   const [value, setValue] = useState(0);
 
+  useEffect(() => {
+    const storedValue = localStorage.getItem('deliveryMethod');
+    if (storedValue !== null) {
+      const i = contents.findIndex(item => item.title === JSON.parse(storedValue).title)
+      setValue(i);
+    }
+  }, []);
+
   const onChange = (e: RadioChangeEvent) => {
     console.log('radio checked', e.target.value);
     setValue(e.target.value);
+
+    const deliveryMethod = contents[e.target.value];
+    localStorage.setItem('deliveryMethod', JSON.stringify(deliveryMethod));
   };
 
   return (
@@ -46,9 +60,9 @@ export const DeliveryMethod = () => {
         >
           <Radio.Group name='method' className='rounded-lg border-gray-400 border-2 w-full' onChange={onChange} value={value}>
             <Space className='w-full' direction="vertical">
-              <MyRadio state={value} value={0} content={contents[0]} icon={transportIcon} />
-              <MyRadio state={value} value={1} content={contents[1]} icon={shopIcon} />
-              <MyRadio state={value} value={2} content={contents[2]} icon={inpostIcon} />
+              {contents.map((content, index) => (
+                <MyRadio key={index} state={value} value={index} content={content} />
+              ))}
             </Space>
           </Radio.Group>
         </ConfigProvider>
