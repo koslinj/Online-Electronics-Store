5b73da4ef4e80a4fd35e213d0e0fa3309e065668
koslinj <koslinj@gmail.com>
2024-04-06 22:41:14 +0200

    add validation to adding and updating addresses

--- src/components/user/orderingData/FormForOrderingData.tsx
+++ src/components/user/orderingData/FormForOrderingData.tsx
@@ -1,9 +1,10 @@
 import { Address, User } from '@/types';
 import { ConfigProvider, Input, Modal, message } from 'antd'
 import axios from 'axios';
-import { Dispatch, FormEvent, SetStateAction, useState } from 'react';
+import { ChangeEvent, Dispatch, FormEvent, SetStateAction, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FaCheckCircle } from 'react-icons/fa';
+import { z } from "zod";
 
 interface Props {
   user: User
@@ -15,15 +16,43 @@ interface Props {
 
 export const FormForOrderingData = ({ user, open, setOpen, onUpdate, address }: Props) => {
   const { t } = useTranslation()
+
+  const ValidAddress = z.object({
+    fullName: z.string().regex(new RegExp(/^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ'"\-\s]+$/), t("badFullName")),
+    phone: z.string().length(9, t("badPhone")).regex(new RegExp(/^[0-9]+$/), t("badPhone")),
+    email: z.string().email(t("badEmail")),
+    street: z.string().min(1, t("required")),
+    zipCode: z.string().min(1, t("required")),
+    city: z.string().min(1, t("required")),
+  });
+  type ValidAddress = z.infer<typeof ValidAddress>;
+
   const [fullName, setFullName] = useState(address?.fullName || "")
   const [street, setStreet] = useState(address?.street || "")
   const [zipCode, setZipCode] = useState(address?.zipCode || "")
   const [city, setCity] = useState(address?.city || "")
   const [phone, setPhone] = useState(address?.phone || "")
   const [email, setEmail] = useState(address?.email || "")
+  const [errors, setErrors] = useState<{ email?: string, phone?: string, fullName?: string, street?: string, zipCode?: string, city?: string }>();
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
+
+    let result = ValidAddress.safeParse({ email: email, phone: phone, fullName: fullName, street: street, zipCode: zipCode, city: city })
+    if (!result.success) {
+      const ob = result.error.flatten().fieldErrors
+      setErrors({
+        email: ob.email?.[0],
+        phone: ob.phone?.[0],
+        fullName: ob.fullName?.[0],
+        street: ob.street?.[0],
+        zipCode: ob.zipCode?.[0],
+        city: ob.city?.[0]
+      });
+      console.log(result.error.flatten().fieldErrors)
+      return;
+    }
+
     const formData = new FormData();
     formData.append('fullName', fullName);
     formData.append('street', street);
@@ -73,6 +102,36 @@ export const FormForOrderingData = ({ user, open, setOpen, onUpdate, address }: 
     setOpen(false);
   };
 
+  const handleFullNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFullName(e.target.value)
+    setErrors({ ...errors, fullName: '' })
+  }
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+    setErrors({ ...errors, email: '' })
+  }
+
+  const handlePhoneChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPhone(e.target.value)
+    setErrors({ ...errors, phone: '' })
+  }
+
+  const handleStreetChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setStreet(e.target.value)
+    setErrors({ ...errors, street: '' })
+  }
+
+  const handleZipCodeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setZipCode(e.target.value)
+    setErrors({ ...errors, zipCode: '' })
+  }
+
+  const handleCityChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value)
+    setErrors({ ...errors, city: '' })
+  }
+
   return (
     <Modal
       title={<p className='text-xl mb-4 font-bold'>{!address ? t('addAddress') : t('editAddress')}</p>}
@@ -100,12 +159,60 @@ export const FormForOrderingData = ({ user, open, setOpen, onUpdate, address }: 
         }}
       >
         <form onSubmit={handleSubmit} className='space-y-6'>
-          <Input placeholder='Imię i nazwisko' value={fullName} onChange={e => setFullName(e.target.value)} />
-          <Input placeholder='Ulica i numer' value={street} onChange={e => setStreet(e.target.value)} />
-          <Input placeholder='Kod pocztowy' value={zipCode} onChange={e => setZipCode(e.target.value)} />
-          <Input placeholder='Miejscowość' value={city} onChange={e => setCity(e.target.value)} />
-          <Input placeholder='Telefon' value={phone} onChange={e => setPhone(e.target.value)} />
-          <Input placeholder='E-mail' value={email} onChange={e => setEmail(e.target.value)} />
+          <div>
+            <Input
+              className={`${errors?.fullName && 'border-red-600 hover:border-red-600'}`}
+              placeholder='Imię i nazwisko'
+              value={fullName}
+              onChange={handleFullNameChange}
+            />
+            <p className="text-red-600 absolute">{errors?.fullName}</p>
+          </div>
+          <div>
+            <Input
+              className={`${errors?.street && 'border-red-600 hover:border-red-600'}`}
+              placeholder='Ulica i numer'
+              value={street}
+              onChange={handleStreetChange}
+            />
+            <p className="text-red-600 absolute">{errors?.street}</p>
+          </div>
+          <div>
+            <Input
+              className={`${errors?.zipCode && 'border-red-600 hover:border-red-600'}`}
+              placeholder='Kod pocztowy'
+              value={zipCode}
+              onChange={handleZipCodeChange}
+            />
+            <p className="text-red-600 absolute">{errors?.zipCode}</p>
+          </div>
+          <div>
+            <Input
+              className={`${errors?.city && 'border-red-600 hover:border-red-600'}`}
+              placeholder='Miejscowość'
+              value={city}
+              onChange={handleCityChange}
+            />
+            <p className="text-red-600 absolute">{errors?.city}</p>
+          </div>
+          <div>
+            <Input
+              className={`${errors?.phone && 'border-red-600 hover:border-red-600'}`}
+              placeholder='Telefon'
+              value={phone}
+              onChange={handlePhoneChange}
+            />
+            <p className="text-red-600 absolute">{errors?.phone}</p>
+          </div>
+          <div>
+            <Input
+              className={`${errors?.email && 'border-red-600 hover:border-red-600'}`}
+              placeholder='E-mail'
+              value={email}
+              onChange={handleEmailChange}
+            />
+            <p className="text-red-600 absolute">{errors?.email}</p>
+          </div>
           <button type='submit' />
         </form>
       </ConfigProvider>
