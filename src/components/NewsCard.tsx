e44806e7f8934d525f36e4c725c159ec76e7bd67
koslinj <koslinj@gmail.com>
2024-04-06 20:31:41 +0200

    center image in NewsCard

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -13,7 +13,7 @@ export const NewsCard = ({ data }: Props) => {
   return (
     <div className='cursor-pointer'>
       <div className='border-2 rounded-xl border-gray-300 px-2'>
-        <img src={data.image} alt="" />
+        <img className='mx-auto' src={data.image} alt="" />
       </div>
       <h3 className='font-semibold text-xl line-clamp-2'>{data.title}</h3>
       <div className='flex items-center gap-2 mt-3'>
