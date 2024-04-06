7310471ded52a531c7fdc1a71512a8649d2c3ab4
koslinj <koslinj@gmail.com>
2024-04-06 14:21:26 +0200

    change titles

--- src/data/data.ts
+++ src/data/data.ts
@@ -10,49 +10,49 @@ import akt8 from "@/data/aktualnosci/akt8.webp";
 export const data = [
   {
     "image": akt1,
-    "title": "First News Title",
+    "title": "Projektujesz grafikę? Poznaj tablety Bosto i zdobądź kod promocyjny",
     "time": "wczoraj",
     "comments": "1 komentarz"
   },
   {
     "image": akt2,
-    "title": "Second News Title",
+    "title": "Doświadcz żywych kolorów i głębokiej czerni z monitorami MSI MPG i MAG QD‑OLED",
     "time": "3 dni temu",
     "comments": "0 komentarzy"
   },
   {
     "image": akt3,
-    "title": "Third News Title",
+    "title": "Kup laptopa z Windows 11 Home i zyskaj pakiet Microsoft Office Home & Student 2021 taniej aż o 100 zł",
     "time": "tydzień temu",
     "comments": "3 komentarze"
   },
   {
     "image": akt4,
-    "title": "Fourth News Title",
+    "title": "Odkryj SanDisk Portable SSD i przechowuj wspomnienia na kieszonkowym dysku",
     "time": "2 tygodnie temu",
     "comments": "0 komentarzy"
   },
   {
     "image": akt5,
-    "title": "Fifth News Title",
+    "title": "Po dobrym przychodzi jeszcze lepsze. Zamień Windowsa 10 na 11",
     "time": "3 tygodnie temu",
     "comments": "2 komentarze"
   },
   {
     "image": akt6,
-    "title": "Sixth News Title",
+    "title": "Komputery z Intel Core 14. generacji do domu i biura. Odkryj moc PC nowej generacji",
     "time": "3 tygodnie temu",
     "comments": "0 komentarzy"
   },
   {
     "image": akt7,
-    "title": "Seventh News Title",
+    "title": "Kup laptop dla nauczyciela w x‑komie i zyskaj atrakcyjne bonusy",
     "time": "3 tygodnie temu",
     "comments": "5 komentarzy"
   },
   {
     "image": akt8,
-    "title": "Eighth News Title",
+    "title": "Premiera Fractal Design North XL. Wyjątkowa obudowa z drewnianym frontem pojemna jak nigdy wcześniej",
     "time": "3 tygodnie temu",
     "comments": "0 komentarzy"
   }
