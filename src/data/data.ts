a1b7a36e88f269e9e6f8fe2252c7258c08d3bfec
koslinj <koslinj@gmail.com>
2024-04-06 13:54:03 +0200

    prepare data object for 'Aktualnosci'

---
+++ src/data/data.ts
@@ -0,0 +1,59 @@
+import akt1 from "@/data/aktualnosci/akt1.webp";
+import akt2 from "@/data/aktualnosci/akt2.webp";
+import akt3 from "@/data/aktualnosci/akt3.webp";
+import akt4 from "@/data/aktualnosci/akt4.webp";
+import akt5 from "@/data/aktualnosci/akt5.webp";
+import akt6 from "@/data/aktualnosci/akt6.webp";
+import akt7 from "@/data/aktualnosci/akt7.webp";
+import akt8 from "@/data/aktualnosci/akt8.webp";
+
+export const data = [
+  {
+    "image": akt1,
+    "title": "First News Title",
+    "time": "wczoraj",
+    "comments": "1 komentarz"
+  },
+  {
+    "image": akt2,
+    "title": "Second News Title",
+    "time": "3 dni temu",
+    "comments": "0 komentarzy"
+  },
+  {
+    "image": akt3,
+    "title": "Third News Title",
+    "time": "tydzień temu",
+    "comments": "3 komentarze"
+  },
+  {
+    "image": akt4,
+    "title": "Fourth News Title",
+    "time": "2 tygodnie temu",
+    "comments": "0 komentarzy"
+  },
+  {
+    "image": akt5,
+    "title": "Fifth News Title",
+    "time": "3 tygodnie temu",
+    "comments": "2 komentarze"
+  },
+  {
+    "image": akt6,
+    "title": "Sixth News Title",
+    "time": "3 tygodnie temu",
+    "comments": "0 komentarzy"
+  },
+  {
+    "image": akt7,
+    "title": "Seventh News Title",
+    "time": "3 tygodnie temu",
+    "comments": "5 komentarzy"
+  },
+  {
+    "image": akt8,
+    "title": "Eighth News Title",
+    "time": "3 tygodnie temu",
+    "comments": "0 komentarzy"
+  }
+];
